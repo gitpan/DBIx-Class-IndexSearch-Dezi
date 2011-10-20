286a09f19aa3829ef2994e36c74c30c584cb8660
Logan Bell <logie@cpan.org>
2011-10-20 02:04:49 +0000

    Import of LOGIE/DBIx-Class-IndexSearch-Dezi-0.02 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-IndexSearch-Dezi
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LOGIE/DBIx-Class-IndexSearch-Dezi-0.02.tar.gz
    gitpan-cpan-author:       LOGIE
    gitpan-cpan-maturity:     released

--- t/sql/sqlite.sql
+++
@@ -1,8 +0,0 @@
-create table person (
-    person_id       integer primary key not null,
-    name            varchar(128) not null,
-    age             integer not null,
-    email           varchar(128),
-    created         timestamp not null    
-);
-
